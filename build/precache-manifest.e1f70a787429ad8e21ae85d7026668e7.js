b763a0b64f14d347ecd0b89f81d6e1a3edb07580
unknown <rahulcon101@gmail.com>
2021-07-17 00:40:45 +0530

    first commit

---
+++ build/precache-manifest.e1f70a787429ad8e21ae85d7026668e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0ce7c6bb01850db7aa6984a16e3a2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dbd0bfa4ce5f7fd0445",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0dbd0bfa4ce5f7fd0445",
+    "url": "/static/js/2.fa0d6e69.chunk.js"
+  },
+  {
+    "revision": "5ab5c0f8420c64f06616e5d1682f6687",
+    "url": "/static/js/2.fa0d6e69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fc7ba87542fb335c4fe",
+    "url": "/static/js/main.0ab6a889.chunk.js"
+  },
+  {
+    "revision": "9af66b5a0c7baacdda91",
+    "url": "/static/js/runtime-main.cf17ad8a.js"
+  },
+  {
+    "revision": "79b4d48d465a4ceb49e6747eb33a4404",
+    "url": "/static/media/background.79b4d48d.jpg"
+  },
+  {
+    "revision": "d60d852e9643287d4065be5e71c70b62",
+    "url": "/static/media/background.d60d852e.jpeg"
+  }
+]);
